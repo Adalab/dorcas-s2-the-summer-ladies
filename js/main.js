9c54559da93a19eb217ca3542517923834eac62e
ashesnei <lau.mc84@gmail.com>
2018-07-02 11:30:45 +0200

    inicial

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 'use strict';
 
 
+var selectorDiseña = document.querySelector('.title__diseña');
+var formularioDisena = document.querySelector(".form__diseña--fontsColors");
 var selectorRellena = document.querySelector('.titulo__rellena');
 var formularioRellena = document.querySelector('.form__rellena');
-var formularioDisena = document.querySelector(".form__diseña--fontsColors");
+var selectorComparte = document.querySelector('.container__comparte--icon');
+var formularioComparte = document.querySelector('.wrapform');
 
+
+
+function desplegarDiseña() {
+    if (formularioDiseña.classList.contains("form__oculto")) {
+      formularioDiseña.classList.remove("form__oculto");
+      formularioRellena.classList.add("form__oculto");
+      formularioComparte.classList.add("form__oculto")
+    }
+    else {
+      formularioDiseña.classList.add("form__oculto")
+    }
+  
+  }
 
 
 function desplegarRellena() {
   if (formularioRellena.classList.contains("form__oculto")) {
     formularioRellena.classList.remove("form__oculto");
-    formularioDisena.classList.add("form__oculto")
+    formularioDisena.classList.add("form__oculto");
+    formularioComparte.classList.add("form__oculto")
   }
   else {
     formularioRellena.classList.add("form__oculto")
@@ -18,4 +35,18 @@ function desplegarRellena() {
 
 }
 
+function desplegarComparte() {
+    if (formularioComparte.classList.contains("form__oculto")) {
+      formularioComparte.classList.remove("form__oculto");
+      formularioDisena.classList.add("form__oculto");
+      formularioRellena.classList.add("form__oculto")
+    }
+    else {
+      formularioComparte.classList.add("form__oculto")
+    }
+  
+  }
+
+selectorDiseña.addEventListener("click", desplegarDiseña);
 selectorRellena.addEventListener("click", desplegarRellena);
+selectorComparte.addEventListener("click", desplegarComparte);
