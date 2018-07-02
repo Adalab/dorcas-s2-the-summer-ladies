07bb98d1710a6e5b589682f63a0c6bbe4464ea0e
ashesnei <lau.mc84@gmail.com>
2018-07-02 10:41:41 +0200

    añadir js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,65 @@
 'use strict';
 
-var selectorRellena = document.querySelector('.titulo__rellena');
-var formularioRellena = document.querySelector('.form__rellena');
+var selectorTitulo = document.querySelectorAll('.wraptitle');
+var formulario = document.querySelectorAll('.wrapform');
 
-
-function plegar() {
-    formularioRellena.classList.toggle("form__oculto");
-    
+function collapsibleall() {
+    formulario.classList.contains("form__oculto");
 }
 
-selectorRellena.addEventListener("click", plegar);
+collapsibleall();
+
+// function updateCollapsibles(e) {
+//     var parents = e.currentTarjet.parentElement;
+
+//     if (parent.classList.contains("form__oculto"))
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function plegar() {
+//     formulario.classList.toggle("form__oculto");
+    
+// }
+
+// selectorTitulo.addEventListener("click", plegar);
+
+
