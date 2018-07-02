b53bf62194a6a16d70361901f40ce0768d5a4baf
andre <andreina.vargas@gmail.com>
2018-07-02 19:43:09 +0200

    adding class form

--- js/main.js
+++ js/main.js
@@ -6,32 +6,35 @@ var formularioDisena = document.querySelector(".form__disena--fontsColors");
 var selectorRellena = document.querySelector(".titulo__rellena");
 var formularioRellena = document.querySelector(".form__rellena");
 var selectorComparte = document.querySelector(".container__comparte--icon");
-var formularioComparte = document.querySelector(".wrapform");
+var formularioComparte = document.querySelector(".form__comparte");
 
 
 
 function desplegarDisena() {
     console.log("desplegardiseña");
     if (formularioDisena.classList.contains("form__oculto")) {
+      console.log(formularioDisena.classList);
       formularioDisena.classList.remove("form__oculto");
       formularioRellena.classList.add("form__oculto");
       formularioComparte.classList.add("form__oculto");
-    }
-    else {
+    } else {
+      console.log(2222)
       formularioDisena.classList.add("form__oculto")
     }
-  
+
   }
 
 
 function desplegarRellena() {
     console.log("desplegarRellena");
   if (formularioRellena.classList.contains("form__oculto")) {
+    console.log(33333)
     formularioRellena.classList.remove("form__oculto");
     formularioDisena.classList.add("form__oculto");
     formularioComparte.classList.add("form__oculto");
   }
   else {
+    console.log(4444)
     formularioRellena.classList.add("form__oculto");
   }
 
@@ -40,14 +43,16 @@ function desplegarRellena() {
 function desplegarComparte() {
     console.log("desplegarComparte");
     if (formularioComparte.classList.contains("form__oculto")) {
+      console.log(5555)
       formularioComparte.classList.remove("form__oculto");
       formularioDisena.classList.add("form__oculto");
       formularioRellena.classList.add("form__oculto");
     }
     else {
+      console.log(66666)
       formularioComparte.classList.add("form__oculto");
     }
-  
+
   }
 
 selectorDisena.addEventListener("click", desplegarDisena);
