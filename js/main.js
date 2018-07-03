0ee373eeda4925bb883e470a98d1581eeaaffaf3
Aylecilla <aylenortiz@gmail.com>
2018-07-03 11:50:04 +0200

    cambios skills

--- js/main.js
+++ js/main.js
@@ -12,27 +12,21 @@ var botonCrearTarjeta = document.querySelector(
 );
 
 function desplegarDisena() {
-  console.log("desplegardiseña");
   if (formularioDisena.classList.contains("form__oculto")) {
-    console.log(formularioDisena.classList);
     formularioDisena.classList.remove("form__oculto");
     formularioRellena.classList.add("form__oculto");
     formularioComparte.classList.add("form__oculto");
   } else {
-    console.log(2222);
     formularioDisena.classList.add("form__oculto");
   }
 }
 
 function desplegarRellena() {
-  console.log("desplegarRellena");
   if (formularioRellena.classList.contains("form__oculto")) {
-    console.log(33333);
     formularioRellena.classList.remove("form__oculto");
     formularioDisena.classList.add("form__oculto");
     formularioComparte.classList.add("form__oculto");
   } else {
-    console.log(4444);
     formularioRellena.classList.add("form__oculto");
   }
 }
