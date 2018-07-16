c86f1cb265dd86c99e4ccff20419b5c93af86dcd
ashesnei <lau.mc84@gmail.com>
2018-07-16 08:51:41 +0200

    descomentado userForm

--- js/main.min.js
+++ js/main.min.js
@@ -47,18 +47,18 @@ var searchGithub = document.querySelector('.rellena__github');
 var responseURL = document.querySelector('.container__comparte-link');
 
 //localStorage
-// var userForm = {
-//   'palette': 1,
-//   'typography': 2,
-//   'name' : 'María García',
-//   'job': 'Front-end developer',
-//   'phone': '+34 666666666',
-//   'email': 'mariagar@example.com',
-//   'linkedin': 'mariagar',
-//   'github': 'mariagar',
-//   'photo': 'data:image/png;base64,2342ba...',
-//   'skills': ['HTML', 'Sass', 'JavaScript']
-// };
+var userForm = {
+  'palette': '1',
+  'typography': '2',
+  'name' : 'María García',
+  'job': 'Front-end developer',
+  'phone': '+34 666666666',
+  'email': 'mariagar@example.com',
+  'linkedin': 'mariagar',
+  'github': 'mariagar',
+  'photo': 'data:image/png;base64,2342ba...',
+  'skills': ['HTML', 'Sass', 'JavaScript']
+};
 /*var userForm = {};
 includeLocalStorage();*/
 
